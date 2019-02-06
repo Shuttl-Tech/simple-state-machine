8d312167e3ecad2bda80b7f4a14d1fee610ee7cb
Dhruv Agarwal <dhruvagga@gmail.com>
2019-02-06 13:20:45 +0530

    Add setup files

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="simple_state_machine",
+    version="0.1",
+    description="Utilities",
+    url="https://github.com/Shuttl-Tech/simple-state-machine",
+    author="Dhruv Agarwal",
+    author_email="dhruv.agarwal@shuttl.com",
+    license="MIT",
+    packages=find_packages(),
+    classifiers=["Programming Language :: Python :: 3.7"],
+    install_requires=[],
+    extras_require={
+        "test": ["pytest"],
+        "dev": ["flake8"],
+    },
+)
